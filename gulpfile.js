897ec4e5eabb90a2841f51adad658ff09af6573d
francesco <frankabbruzzese@gmail.com>
2016-06-25 11:15:56 +0200

    changes for debugging package

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ var gulp = require("gulp"),
     grep = require("gulp-grep-contents");
 
     var toprependViewImport = '@using MvcControlsToolkit.Core.Views \n'
-    +'@using MvcControlsToolkit.Core.Transformations \n';
+    +'@using MvcControlsToolkit.Core.Transformations \n'
+    +'@using MvcControlsToolkit.Core.HtmlHelpers \n';
     var topappendViewImport = '\n @addTagHelper *, MvcControlsToolkit.Core'
     +'\n @addTagHelper *, MvcControlsToolkit.ControlsCore';
     var toPrependGulpfile = "require('gulp-load-subtasks')('tasks'); \n";
@@ -15,14 +16,14 @@ var gulp = require("gulp"),
 
     gulp.task('modify:viewimport', function () {
         return gulp.src(["../../Views/_ViewImports.cshtml"], { base: '../..' })
-        .pipe(grep(/MvcControlsToolkit.Core/, {inverted: true}))
+        
         .pipe(insert.wrap(toprependViewImport, topappendViewImport))
     .pipe(gulp.dest("../.."));
     });
 
     gulp.task('modify:gulpfile', function () {
         return gulp.src(["../../gulpfile.js"], { base: '../..' })
-        .pipe(grep(/gulp-load-subtasks/, {inverted: true}))
+        
         .pipe(insert.prepend(toPrependGulpfile))
     .pipe(gulp.dest("../.."));
     });
